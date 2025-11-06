67bf2d59f655c0a9e9ce1dba2d4e68b408506a38
jareddelgado454 <jareddelgadotejada.41@gmail.com>
2025-11-06 12:44:00 -0500

    authentication-completed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { envs } from './config';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+
+  const logger = new Logger("Auth-microservice");
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers
+      }
+    }
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true
+    })
+  );
+
+  await app.listen();
+  logger.log(`Auth microservice running on port ${envs.port}`);
 }
 bootstrap();
